6c3abd9919a92cc94dd12e9dff54bf7e56eaae0a
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2023-03-30 15:00:19 +0200

    update
    ToDolist Completed

--- 10. Tadolist-v1/app.js
+++ 10. Tadolist-v1/app.js
@@ -1,35 +1,58 @@
 const express  = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js"); 
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
 
+console.log(date);
 
 app.get("/", function(req, res){
 
-    let today = new Date();
+    const day = date.getDate();
 
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
-app.post("/", function(req, res){
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
     let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/")
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+
+
+app.post("/", function(req, res){
+    
+    let item = req.body.newItem;
+    
+    if (req.body.list === "Work"){
+        workItems.push(item)
+        res.redirect("/work")
+    } else{
+        items.push(item);
+        res.redirect("/");
+    }
+
     items.push(item);
     res.redirect("/")
 })
+
 
 
 
